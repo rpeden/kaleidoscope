bd7c5932832b9e238747dfb8678b90eaf0080a0e
Ryan Peden <ryanpeden@Ryans-Air.lan>
2014-11-29 20:26:27 -0500

    added token enum

---
+++ kaleidoscope.cc
@@ -0,0 +1,16 @@
+
+/****
+Lexer
+The lexer returns tokens [0-255] if it encounters an unknown character,
+otherwise it returns one of these enum values for things it knows
+about
+****/
+enum Token {
+	tok_eof = -1,
+	//commands
+	tok_def = -2,
+	tok_extern = -3,
+	//primary
+	tok_identifier = -4,
+	tok_def = -5,
+};
