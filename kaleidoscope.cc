5d512760870e0042e13d1218a1bc3012f1135a16
Ryan Peden <ryan04@gmail.com>
2014-11-29 20:38:51 -0500

    added function to get token and gave it the ability to recognize identifiers

--- kaleidoscope.cc
+++ kaleidoscope.cc
@@ -17,3 +17,24 @@ enum Token {
 
 static std::string IdentifierStr;  //Filled in if tok_identifier
 static double NumVal;			   //Filled in if tok_number
+
+/// gettok - return the next token from standard input
+static int gettok() {
+	static int LastChar = ' ';
+
+	//skip any white space
+	while (isspace(LastChar)) {
+		LastChar = getchar();
+	}
+
+	if(isalpha(LastChar)) {  //identifier: [a-zA-Z][a-zA-Z0-9]*
+		IdentifierStr = LastChar;
+		while(isalnum((LastChar = getchar()))) {
+			IdentifierStr += LastChar;
+		}
+
+		if(IdentifierStr == "def") return tok_def;
+		if(IdentifierStr == "extern") return tok_extern;
+		return tok_identifier;
+	}
+}
