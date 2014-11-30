8e4fe2e63a35bb1bc16276cdeacc3bb1cc7e4b86
Ryan Peden <ryan04@gmail.com>
2014-11-29 21:12:51 -0500

    added expression class for function calls

--- kaleidoscope.cc
+++ kaleidoscope.cc
@@ -105,3 +105,12 @@ public:
 	BinaryExprAST(char op, ExprAST *lhs, ExprAST *rhs)
 		: Op(op), LHS(lhs), RHS(rhs)    {}
 };
+
+/// CallExprAST - Expression class for function calls
+class CallExprAST : public ExprAST {
+	std::string Callee;
+	std::vector<ExprAST*> Args;
+public: 
+	CallExprAST(const std::string &callee, std::vector<ExprAST*> &args)
+		: Callee(callee), Args(args)  {}
+}
