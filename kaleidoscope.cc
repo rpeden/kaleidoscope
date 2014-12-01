63acc2df5a5066bf7b45f40afcb0cb85021260d7
Ryan Peden <ryan04@gmail.com>
2014-12-01 06:19:01 -0500

    completed identifier parser function

--- kaleidoscope.cc
+++ kaleidoscope.cc
@@ -193,4 +193,26 @@ static ExprAST *ParseIdentifierExpr() {
 		//simple variable ref
 		return new VariableExprAST(IdName);
 	}
+
+	//Call
+	getNextToken();  //eat (
+	std::vector<ExprAST*> Args;
+	if(CurTok != ')') {
+		while(1) {
+			ExprAST *Arg = ParseExpression();
+			if (!Arg) return 0;
+			Args.push_back(Arg);
+
+			if (CurTok == ')') break;
+
+			if (CurTok != ',')
+				return Error("Expected ')' or ',' in argument list");
+			getNextToken();
+		}
+	}
+
+	//Eat the )
+	getNextToken();
+
+	return new CallExprAST(IdName, Args);
 }
