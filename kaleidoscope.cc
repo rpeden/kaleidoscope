85ba29898fdbe6c2154f9516be3e43297e38fa4d
Ryan Peden <ryan04@gmail.com>
2014-12-01 20:19:36 -0500

    added missing semicolon

--- kaleidoscope.cc
+++ kaleidoscope.cc
@@ -224,7 +224,7 @@ static ExprAST *ParseIdentifierExpr() {
 static ExprAST *ParsePrimary() {
 	switch (CurTok){
 		default: return Error("unknown token when expecting an expression");
-		case tok_identifier: return ParseIdentifierExpr()
+		case tok_identifier: return ParseIdentifierExpr();
 		case tok_number:     return ParseNumberExpr();
 		case '(': 			 return ParseParenExpr();
 	}
