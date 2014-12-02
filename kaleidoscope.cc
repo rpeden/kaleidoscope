78ace7f05a4ee71d9c409ddb02488b13fe2a5532
Ryan Peden <ryan04@gmail.com>
2014-12-01 19:52:09 -0500

    completed ParsePrimary function

--- kaleidoscope.cc
+++ kaleidoscope.cc
@@ -222,5 +222,10 @@ static ExprAST *ParseIdentifierExpr() {
 ///  ::= numberexpr
 ///  ::= parenexpr
 static ExprAST *ParsePrimary() {
-
+	switch (CurTok){
+		default: return Error("unknown token when expecting an expression");
+		case tok_identifier: return ParseIdentifierExpr()
+		case tok_number:     return ParseNumberExpr();
+		case '(': 			 return ParseParenExpr();
+	}
 }
